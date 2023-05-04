3907ba4a0c4e17c7232f9a09004e5129912373b8
Michelle Orbezo <michelleorbezo@Michelles-MacBook-Air.local>
2023-05-03 22:03:36 -0400

    API get and post comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,22 +1,19 @@
 // Global array that stores all comments
-// Pre-filled with 3 comments to start
-let commentArray = [
-    {
-        name: "Connor Walton",
-        timestamp: "02/17/2021",
-        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-    },
-    {
-        name: "Emilie Beach",
-        timestamp: "01/09/2021",
-        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-    },
-    {
-        name: "Miles Acosta",
-        timestamp: "12/20/2020",
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-    } 
-];
+let commentArray = [];
+let getCommentArray = () => {
+    const promise = axios.get("https://project-1-api.herokuapp.com/comments?api_key=c9501e69-3b36-44fe-82c2-9b085178140d");
+    promise.then((response) => {
+        let responseData = response.data;
+        console.log(responseData);
+        commentArray = responseData.reverse();
+        for (let i = 0; i < commentArray.length; i++) {
+            let date = new Date(commentArray[i].timestamp);
+            commentArray[i].timestamp = date.toLocaleDateString();
+        }
+        renderComments();
+    });
+};
+getCommentArray();
 //Functions
 
 // Event handler for form submission
@@ -28,11 +25,19 @@ function formSubmit (event) {
         comment: formFields.comment.value,
         timestamp: new Date().toLocaleDateString("en-US")
     };
-
-    commentArray.unshift (comment);
-    renderComments ();
+    const postComment = axios.post("https://project-1-api.herokuapp.com/comments?api_key=c9501e69-3b36-44fe-82c2-9b085178140d", 
+        {
+            name: comment.name,
+            comment: comment.comment
+        }
+    );
+    postComment.then((response) => {
+        const postedComment = response.data;
+        commentArray.unshift (comment);
+        renderComments ();
+    });
     event.target.reset();
-}
+};
 // function that displays all comments in commentArray in the HTML
 function renderComments () {
     const display = document.querySelector(".comments__render");
@@ -44,7 +49,7 @@ function renderComments () {
         const divider = document.createElement("HR");
         display.appendChild(divider);
     }
-}
+};
 // function that creates an HTML Element from a comment object from the array
 function createCommentHtml (comment) {
     const commentDisplay = document.createElement("div");
@@ -72,6 +77,6 @@ function createCommentHtml (comment) {
     commentDisplay.appendChild(text);
 
     return commentDisplay;
-}
+};
 const form = document.getElementById("comment-form");
 form.addEventListener("submit", formSubmit);
